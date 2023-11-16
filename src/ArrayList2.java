c75f34f65581ad6526806e1921b722d00fae5e0b
Christober <christobersg@gmail.com>
2023-11-16 07:39:02 +0530

    Created new array list

--- src/ArrayList2.java
+++ src/ArrayList2.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 public class ArrayList2 {
     public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+
+        //Created arrayList and added new values
+        al.add(9);
+        al.add(5);
+        al.add(19);
+        al.add(2);
+        al.add(1);
+        al.add(20);
+        al.add(4);
+
+        //printing values of arraylist
+        System.out.println(al);
+
+        //Created new arrayList
+        ArrayList al2 = new ArrayList<>();
+        al2.addAll(al);
+
+        System.out.println(al2);
 
     }
 }
